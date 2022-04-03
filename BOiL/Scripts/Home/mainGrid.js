a1fe7cc92c96ab065fc4cb9efe99197226af0093
Daniel Sochacki <37577115+daniels-aurea@users.noreply.github.com>
2022-04-03 17:12:02 +0200

    Michal's final fixes

--- BOiL/Scripts/Home/mainGrid.js
+++ BOiL/Scripts/Home/mainGrid.js
@@ -1,6 +1,6 @@
 ﻿var currentValue = 0;
 var error = false;
-
+var MAXEET = 0;
 $(document).ready(function () {
     console.log("ready!");
 
@@ -55,24 +55,36 @@ function critialPath() {
     var nodes = new vis.DataSet([]);
     var idsOnCritialPath = [];
     var idsOnCritialPathCount = 0;
+    var maxEet = 0;
 
+    var count = 0;
+    var names = [];
     for (var i = 0; i < data.length; i++) {
         var item = data[i];
         if (item.OnCriticalPath) {
-            nodes.add({ id: item.Id, label: item.Id.toString(), x: idsOnCritialPathCount* 100, y:100 });
-            idsOnCritialPath[idsOnCritialPathCount] = item.Id;
-            idsOnCritialPathCount++;
+            maxEet = item.Eet;
+            names.push(item.Name);
+            count++;
         }
     }
+
+    for (var i = 0; i < count + 1; i++) {
+        nodes.add({ id: i, label: "", x: idsOnCritialPathCount * 100, y: 100 });
+        idsOnCritialPath[idsOnCritialPathCount] = i;
+        idsOnCritialPathCount++;
+    }
+    MAXEET = maxEet;
+    console.log(MAXEET);
+    document.getElementById("EetText").innerHTML = "Critical Path time: " + maxEet;
 
     var edges = new vis.DataSet([
     ]);
 
 
     for (var i = 1; i < idsOnCritialPathCount; i++) {
-        edges.add({ from: idsOnCritialPath[i - 1], to: idsOnCritialPath[i], arrow: "to" });
+        edges.add({ from: idsOnCritialPath[i - 1], to: idsOnCritialPath[i], arrow: "to", label: names[i - 1] });
     }
-    
+
 
     // create a network
     var container = document.getElementById('critialPath');
